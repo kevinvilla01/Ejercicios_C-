5852bf8b2bc8c19654f0776f8233f2a15068a5a1
Kevin Villa <90355499+kevinvilla01@users.noreply.github.com>
2024-01-19 14:17:08 -0600

    Add files via upload

---
+++ practica_1.14/practica_1.14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_1._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Preguntar edad y calcular cuantos dias ha vivido
+            // edad * 365
+
+            int edad = 0, res = 0;
+
+            Console.WriteLine("Cuantos anios tienes?");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            res = edad * 365;
+            Console.WriteLine("Has vivido {0} dias", res);
+
+            Console.ReadKey();
+        }
+    }
+}
