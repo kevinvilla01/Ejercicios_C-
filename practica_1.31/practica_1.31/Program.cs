00c7d3c48744694c4096ef22ef1a0682a9bb2486
Kevin Villa <90355499+kevinvilla01@users.noreply.github.com>
2024-02-09 09:31:36 -0600

    ejercicios

---
+++ practica_1.31/practica_1.31/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_1._31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Switch case detector de putos
+
+            char genero;
+            Console.WriteLine("Escriba su genero: H = Hombre M = Mujer");
+            genero = Convert.ToChar(Console.ReadLine());
+
+            switch (genero)
+            {
+                case 'h':
+                    Console.WriteLine("eres hombre we");
+                    break;
+
+                case 'H':
+                    Console.WriteLine("Eres hobre we");
+                    break;
+
+                case 'm':
+                    Console.WriteLine("Eres mujer we");
+                    break;
+
+                case 'M': 
+                    Console.WriteLine("Eres mujer we");
+                    break;
+
+                default:
+                    Console.WriteLine("GAY");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ practica_1.32/practica_1.32/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_1._32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Obtener un numero entre 1 y siete y mostrar el dia de la senana correspondiente
+
+
+            int day = 0;
+            Console.WriteLine("Escriba un numero entre 1 y 7");
+            day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Miercoles");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Sabado");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Domingo");
+                    break;
+
+
+                default:
+                    Console.WriteLine("1 al 7 pendejo");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
